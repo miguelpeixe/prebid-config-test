93429c4e18d5d2db78692a1873883afc22bbf965
Miguel Peixe <miguel@peixe.co>
2022-01-26 12:00:35 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+    prebid: "./src/prebid.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /.js$/,
+        include: new RegExp(`\\${path.sep}prebid\\.js`),
+        use: {
+          loader: "babel-loader",
+          options: {
+            ...require("prebid.js/.babelrc.js"),
+            configFile: true,
+          },
+        },
+      },
+    ],
+  },
+};
